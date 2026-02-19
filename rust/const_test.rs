e71fd056a86f01f7afe606b19cbb5b5391f87ed9
DUDLEYDANIEL <dudleydaniel.raj2005@gmail.com>
2026-02-19 11:32:55 +0530

    studies about box, linkedlist, constant

---
+++ rust/const_test.rs
@@ -0,0 +1,17 @@
+
+// Global declared all scope 
+static LANGUAGE: &str = "RUST";
+const THRESHOLD: i32 = 10;
+
+fn is_big(n: i32)
+    -> bool {
+    n > THRESHOLD
+}
+
+fn main() {
+    let n = 16;
+
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else {"small"});
+}
